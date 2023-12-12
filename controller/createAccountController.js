1ec1e9df342734a76ab6b321b8544d7cf546357b
yadragon2007 <102539850+yadragon2007@users.noreply.github.com>
2023-12-12 10:59:37 +0200

    add create account page

--- controller/createAccountController.js
+++ controller/createAccountController.js
@@ -8,11 +8,32 @@ const createAcc_createAcc_get = (req, res) => {
 const createAcc_createAcc_post = async (req, res) => {
   const { userName, password } = req.body;
   // Check if username is already in use
-  const Account = await Accounts.find({ userName: userName}).exec();
-  if(Account == []){
-    
-  }else{
-    console.log('err')
+  const Account = await Accounts.find({ userName: userName }).exec();
+  console.log(Account)
+  if (Account.length == 0) {
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // data that will send to data base
+    const userDataInputs = {
+      userName,
+      password: hashedPassword,
+      notes: [],
+    };
+    const account = new Accounts(userDataInputs);
+    // save data on data base
+    await account.save();
+    // get user Data from data base
+    const userData = await Accounts.findOne({ userName: userName });
+    // add cookies
+    res.cookie("userData", userData, {
+      secure: true,
+      maxAge: 999999999999999,
+    });
+    // redirect
+    res.redirect('/')
+  } else {
+    console.log("err");
   }
 };
 
